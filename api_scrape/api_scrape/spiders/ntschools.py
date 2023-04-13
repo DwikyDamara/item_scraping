570d3436fb4ff005885aa06b2269233e204c23a7
Dwiky Damara <damaradwiky6@gmail.com>
2023-04-13 16:06:14 +0700

    School data scrape created

---
+++ api_scrape/api_scrape/spiders/ntschools.py
@@ -0,0 +1,47 @@
+import scrapy
+import json
+
+
+class NtschoolsSpider(scrapy.Spider):
+    name = "ntschools"
+    start_urls = ["https://directory.ntschools.net/#/schools"]
+    headers = {
+        "Accept": "application / json",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US, en; q=0.9",
+        "Referer": "https://directory.ntschools.net/",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same - origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+        "X-Requested-With": "Fetch"
+    }
+
+    def parse(self, response):
+        url = "https://directory.ntschools.net/api/System/GetAllSchools"
+        request = scrapy.Request(url,
+                callback=self.parse_api,
+                headers=self.headers)
+        yield request
+
+    def parse_api(self, response):
+        base_url="https://directory.ntschools.net/api/System/GetSchool?itSchoolCode="
+        raw = response.body
+        data = json.loads(raw)
+        for school in data:
+            school_code = school["itSchoolCode"]
+            school_url= base_url+school_code
+            request = scrapy.Request(school_url,
+                callback=self.parse_school,
+                headers=self.headers)
+            yield request
+
+    def parse_school(self, response):
+        raw_data = response.body
+        data = json.loads(raw_data)
+        yield {
+            "name" : data["name"],
+            "Address" : data["physicalAddress"]["displayAddress"],
+            "Postal Address" : data["postalAddress"]["displayAddress"],
+            "E-mail" : data["mail"],
+            "Phone" : data["telephoneNumber"]
+        }
